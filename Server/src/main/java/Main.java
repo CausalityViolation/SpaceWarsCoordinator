78e26d3b32df1c00e09cdb11da7ae0e81470c1a1
Martin Siönäs <martin.sionas@gmail.com>
2021-05-27 13:36:18 +0200

    Continued work on Serverbranch

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -1,2 +1,21 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try (ServerSocket serverSocket = new ServerSocket(5050)) {
+            while (true) {
+                Socket client = serverSocket.accept();
+                executorService.submit(() -> server.handleConnection(client));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,2 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+    public void handleConnection(Socket client) {
+        BufferedReader inputFromClient = null;
+        try {
+            inputFromClient = new BufferedReader(new InputStreamReader((client.getInputStream())));
+            readRequest(inputFromClient);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readRequest(BufferedReader inputFromClient) throws IOException {
+        List<String> tempList = new ArrayList<>();
+
+        while (true) {
+            var line = inputFromClient.readLine();
+            if (line == null || line.isEmpty()) {
+                break;
+            }
+            tempList.add(line);
+            System.out.println(line);
+        }
+    }
 }
